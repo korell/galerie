6e8c88cf52058cd25f684a565030019c787cde70
Matthieu Rébillard <matthieurebillard@yahoo.fr>
2014-05-19 15:24:29 +0200

    Mise en place de l'admin

---
+++ admin/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html class="<?= CLASSPAGE?>">
+<head>
+    <title><?= galerieTitre()?> : accueil</title>
+    <link rel="stylesheet" href="../css/reset.css"/>
+    <link rel="stylesheet" href="../css/style.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <meta charset="utf-8" />
+</head>
+<body>
--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,21 @@
-<?php 
-	include('../header.php');
+<?php
+	include('../init.php');
+	include('header.php');
+	$images = getListImg();
+	$table = '<div class="admin"><table>';
+	foreach ($images as $image){
+		$table .= '<tr>';
+		$table .= '<td><img src="../'.galerieImgDirectory().'/'.$image['nom_fichier'].'" alt="image" ></td>';
+		$table .= '<td>'.$image['titre'].'</td>';
+		$table .= '<td>'.$image['description'].'</td>';
+		$table .= '<td>'.$image['auteur'].'</td>';
+		$table .= '<td>'.$image['nom_fichier'].'</td>';
+		$table .= '<td>'.$image['date_ajout'].'</td>';
+		$table .= '</tr>';
+	}
+	$table .= '</table>';
+	echo $table;
+
  ?>
 <form method="post" enctype="multipart/form-data">
 	<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
